08d8ab37d19fc34491b605465574d926d1e391f0
p3javier <65142395+p3javier@users.noreply.github.com>
2021-02-06 02:07:56 +0100

    files and folders modified, navbar personalized

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./App.css";
 
+import NavbarMenu from "./components/NavbarMenu/NavbarMenu";
+
 function App() {
   const [toggle, setToggle] = React.useState<boolean>(false);
   console.log("toggle", toggle);
@@ -10,13 +12,16 @@ function App() {
     ? "nm-flat-red-500-lg rounded-full hover:nm-convex-red-500-lg rounded-full"
     : "bg-blue-500 hover:bg-blue-500";
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button
-        className={`p-3 rounded-sm ${buttonClasses}`}
-        onClick={() => setToggle(!toggle)}
-      >
-        Toggle dsfdssdfdsfsdfdsfsdf
-      </button>
+    <div>
+      <NavbarMenu />
+      <div className="flex items-center justify-center h-screen">
+        <button
+          className={`p-3 rounded-sm ${buttonClasses}`}
+          onClick={() => setToggle(!toggle)}
+        >
+          Toggle dsfdssdfdsfsdfdsfsdf
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavbarMenu/NavbarLinks/Contact.tsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <a
+      href="#"
+      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+    >
+      Calendar
+    </a>
+  );
+};
+
+export default Contact;
