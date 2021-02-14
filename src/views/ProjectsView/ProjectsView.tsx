7cf504b3bc0b56ac44f8cc1fefaa86742d6e882b
p3javier <65142395+p3javier@users.noreply.github.com>
2021-02-14 23:58:51 +0100

    projects

--- src/views/ProjectsView/ProjectsView.tsx
+++ src/views/ProjectsView/ProjectsView.tsx
@@ -1,5 +1,10 @@
 const ProjectsView = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>{"project  1"}</div>
+      <div>{"project  2"}</div>
+    </div>
+  );
 };
 
 export default ProjectsView;
