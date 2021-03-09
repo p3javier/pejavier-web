4ce72ee0ed4c85f02c59be8fa015be6b3175af4c
p3javier <65142395+p3javier@users.noreply.github.com>
2021-03-09 19:47:48 +0100

    manifest.json modified

--- src/components/ImageProfile/ImageProfile.tsx
+++ src/components/ImageProfile/ImageProfile.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-//import image from "assets/img/avatar.webp";
-
-//import image from "../../../public/android-chrome-192x192.png";
 const ImageProfile = () => {
   const [translate, setTranslate] = useState("-translate-y-96");
 
@@ -12,7 +9,7 @@ const ImageProfile = () => {
 
   return (
     <img
-      src="android-chrome-192x192.png"
+      src="images/avatar.webp"
       alt="Javier Perez"
       className={`z-0 rounded-full w-40 h-40 max-w-xs border-8 duration-2000 relative transform transition-all ${translate} ease-out`}
     />
